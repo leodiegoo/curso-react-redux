5a9ee9efec96712bfd42c860188bbb5596246018
Leonardo Diego <leoosinhu@gmail.com>
2017-05-30 10:12:16 -0300

    Terminado de configurar o backend da aplicação e Criando o frontend

---
+++ my-money-app/backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = (req, res, next) => {
+    res.header('ACCESS-CONTROL-ALLOW-ORIGIN', '*')
+    res.header('ACCESS-CONTROL-ALLOW-METHODS', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('ACCESS-CONTROL-ALLOW-HEADERS', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+}
