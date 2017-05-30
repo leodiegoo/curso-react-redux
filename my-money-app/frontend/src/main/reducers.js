336ee5e60c71a4ae69e73be0a8170d64f53d2124
Leonardo Diego <leoosinhu@gmail.com>
2017-05-30 15:34:51 -0300

    Criado o frontend da aplicação "my-money-app" utilizando o template admin lte

---
+++ my-money-app/frontend/src/main/reducers.js
@@ -0,0 +1,13 @@
+import {
+    combineReducers
+} from 'redux'
+
+import DashboardReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabRecuder'
+
+const rootReducer = combineReducers({
+    dashboard: DashboardReducer,
+    tab: TabReducer
+})
+
+export default rootReducer
